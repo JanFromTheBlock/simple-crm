25af77151034883437507270f52ffa9db98dd2c6
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2024-02-14 15:50:01 +0100

    NgModel

--- simple-crm/src/app/dialog-add-user/dialog-add-user.component.ts
+++ simple-crm/src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,19 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { MatDialog , MatDialogModule} from '@angular/material/dialog';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { User } from '../../models/user.class';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [MatDialogModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule],
+  imports: [MatDialogModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, FormsModule],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss',
 })
 export class DialogAddUserComponent {
-  constructor(public dialog: MatDialog){
 
+  user = new User();
+  birthDate!: Date;
+
+  constructor(public dialog: MatDialog){
+  }
+
+  saveUser(){
+    this.user.birthDate = this.birthDate.getTime();
+    console.log('Current User is', this.user)
   }
 }
