9afc77e71aae5f802efe53023bee4d8f402d2eee
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2024-02-15 11:46:44 +0100

    show added users with ngmodel

--- simple-crm/src/app/user/user.component.ts
+++ simple-crm/src/app/user/user.component.ts
@@ -7,19 +7,31 @@ import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.compo
 import { MatNativeDateModule } from '@angular/material/core';
 import { User } from '../../models/user.class';
 import {MatCardModule} from '@angular/material/card';
+import { CollectionReference, DocumentData, addDoc, collection, deleteDoc, doc, updateDoc, } from '@firebase/firestore';
+import { Firestore, collectionData, docData, onSnapshot } from '@angular/fire/firestore';
+
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule, MatNativeDateModule, MatCardModule],
+  imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule, MatNativeDateModule, MatCardModule, CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
 
   user = new User();
+  allUsers: any = [];
 
-  constructor(public dialog: MatDialog,) {}
+  constructor(public dialog: MatDialog, private readonly firestore: Firestore) {
+    onSnapshot(collection(this.firestore, 'users'), (list) => {
+      this.allUsers = list.docs.map(doc => doc.data());
+    });
+  }
+
 
   openDialog(){
     this.dialog.open(DialogAddUserComponent);
